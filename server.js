54d7860cf0be51b241c4e9de7b213a4ebd00256f
ScribbleBox <dave.hilborn@gmail.com>
2017-06-13 20:30:22 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+// Handlebars setup
+hbs.registerPartials(__dirname + '/views/Partials');
+app.set('view engine', 'hbs');
+
+// Middleware //
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', `${log} \n`, (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(`${__dirname} ${'/public'}`));
+
+// Handlebar helpers
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => text.toUpperCase());
+
+// Express routes
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hi im booooob'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'bad request'
+  });
+});
+
+// Express server start
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
