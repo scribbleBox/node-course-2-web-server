463fecea933c073a379c9a0b31c25f517bd8bc78
ScribbleBox <dave.hilborn@gmail.com>
2017-06-22 20:23:30 +0100

    Add project page

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 // Handlebars setup
-hbs.registerPartials(__dirname + '/views/Partials');
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 // Middleware //
@@ -44,6 +44,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
+  });
+});
+
+app.get('/projects', (rec, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'projects Page',
   });
 });
 
